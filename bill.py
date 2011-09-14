fa726c5c530e2d50021c083a3f9432db884153fd
Roman Kovalev <rnax@mail.ru>
2011-09-14 23:38:58 +0400

    able to parce Out Of Service info

--- bill.py
+++ bill.py
@@ -11,6 +11,17 @@ g.setup(debug=True)
 
 BASE_URL="http://billing.qwerty.ru/pls/rac.q/!w3_p_main.showform"
 
+def getServiceUnError(body):
+    from lxml.html import fromstring
+    # Дятлы, не выставили кодировку, придется парсить вручную. 
+    # Но мы то знаем что там отстойный cp1251
+    errtext = fromstring(body.decode('cp1251').lower()).xpath('//body/font/h1')
+    if len(errtext) > 0:
+        return errtext[0].text.encode('utf8')
+    else:
+        return None
+    
+
 def get_user_info(user, passwd):
     g.setup(post={'USERNAME': user, 'PASSWORD': passwd, 'IDENTIFICATION': 'CONTRACT', 'FORMNAME': 'QFRAME', 'button': 'Вход'})
     res = g.go(BASE_URL + '?CONFIG=CONTRACT')
@@ -24,7 +35,8 @@ def get_user_info(user, passwd):
         if alert_pos > 0:
             print >> sys.stderr, res.body[alert_pos + 8: res.body.find('"', alert_pos + 8) ].decode('cp1251').encode('utf8')
         else:
-            print >> sys.stderr, 'unable to parse qwerty answer!'
+            errtext = getServiceUnError(res.body)
+            print >> sys.stderr, errtext and errtext or 'unable to parse qwerty answer!'
         return False
 
 
